de3999a4b78c0dd1e327ddf3f7eddf955e8d6435
Jan Schnasse <jan@schnasse.eu>
2018-01-26 10:02:37 +0100

    bug fixes - zgrep .* returned exit 1 because of intial set -e ... - zgrep printed filename to output - add year to search pattern

--- import-logfiles.sh
+++ import-logfiles.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
-set -euo pipefail
-IFS=$'\n\t'
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scriptdir
 source variables.conf
 
-YESTERDAY=$(date +"%d/%b" -d"yesterday")    
+YESTERDAY=$(date +"%d/%b/%Y" -d"yesterday")    
 TMP=$REGAL_TMP
 IDSITE=4
 STATS_LOG=$REGAL_LOGS/stats.log
 
 function loadLogFile() {
     PATTERN=$1
-    zgrep $PATTERN /var/log/apache2/other_vhosts_access.log.* > $TMP/matomoImport.log
+    zgrep --no-filename $PATTERN /var/log/apache2/other_vhosts_access.log.* > $TMP/matomoImport.log
     python $MATOMO/misc/log-analytics/import_logs.py --recorder-max-payload-size=200 --url $MATOMO_URL --login $MATOMO_ADMIN --password $MATOMO_PASSWORD $TMP/matomoImport.log --idsite=$IDSITE >> $STATS_LOG
 }
 
-loadLogFile "$YESTERDAY/[0-9][0-9][0-9][0-9]"
+loadLogFile "$YESTERDAY"
 
 cd -
 # Beispiel um die Logfiles der letzten 24 Tage zu laden
 #
-#for i in {1..24};do echo loadLogFile "$(date +"%d/%b" -d"$i days ago")/[0-9][0-9][0-9][0-9]";done
+#for i in {1..24};do echo loadLogFile "$(date +"%d/%b/%Y" -d"$i days ago")";done
 
