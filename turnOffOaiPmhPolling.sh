ca8da7308c1d3e2df9ca6eeb29c02f6a6bb358e9
Jan Schnasse <jan@schnasse.eu>
2018-01-26 08:40:07 +0100

    v0.1

---
+++ turnOffOaiPmhPolling.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $scriptdir
+source variables.conf
+
+mysql -uproai -p$REGAL_PASSWORD -e"UPDATE proai.rcAdmin SET pollingEnabled=0;"
+
+cd -
