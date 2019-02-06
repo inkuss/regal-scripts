8168640acd806b419ba3eaed6c99f96158c22307
Edoweb-Test <root@edoweb-test.hbz-nrw.de>
2019-02-06 16:37:25 +0100

    parametrize regal password in OAI-PMH polling scripts

--- turnOnOaiPmhPolling.sh
+++ turnOnOaiPmhPolling.sh
@@ -4,6 +4,5 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scriptdir
 source variables.conf
 
-mysql -uproai -p$REGAL_PASSWORD -e"UPDATE proai.rcAdmin SET pollingEnabled=1;"
+mysql -uproai -pproaipwd -e"UPDATE proai.rcAdmin SET pollingEnabled=1;"
 
-cd -
