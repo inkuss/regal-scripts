a81500bba719149b7f0a16144a2a12127c5b5f53
edoweb user <edoweb@edoweb-rlp.de>
2019-12-19 19:14:29 +0100

    Abgleich Produktion <-> master auch für backup-es.sh und backup-db.sh    - Damit sind nun alle Skripte in regal-scripts abgeglichen    - und auch sind alle Skripte aus cronjobs/ nach regal-scripts/ verschoben worden, die in der crontab produktiv in Benutzung sind    - das Verzeichnis /opt/regal/cronjobs enthält also keine produktiven Skripte mehr und muss daher auch nicht versioniert werden, alle relevanten Skripte sind in regal-scripts/

--- backup-db.sh
+++ backup-db.sh
@@ -25,13 +25,13 @@ function clean(){
     num=`expr $len - $LIMIT`
     if [ $num -gt 0 ]
     then
-	SNAPSHOTS=`ls -tr $REGAL_BACKUP/mysql/|head -$num` 
-	# Loop over the results and delete each snapshot
-	for SNAPSHOT in $SNAPSHOTS
-	do
-	    echo "Deleting snapshot: $SNAPSHOT"
-	    rm -rf $REGAL_BACKUP/mysql/$SNAPSHOT
-	done
+        SNAPSHOTS=`ls -tr $REGAL_BACKUP/mysql/|head -$num` 
+        # Loop over the results and delete each snapshot
+        for SNAPSHOT in $SNAPSHOTS
+        do
+            echo "Deleting snapshot: $SNAPSHOT"
+            rm -rf $REGAL_BACKUP/mysql/$SNAPSHOT
+        done
     fi
     echo "Done!"
 }
@@ -41,6 +41,12 @@ function restore(){
     mysql -u root $REGAL_PASSWORD -p < $SNAPSHOT.sql
 }
 
+# Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to -gt 0 the /etc/hosts part is not recognized ( may be a bug )
 while [[ $# -gt 0 ]]
 do
 key="$1"
